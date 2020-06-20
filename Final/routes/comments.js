1ba1afe55118592a570c9fb0b5fde5ada8d13dd2
Paul Macfarlane <pj16macf@siena.edu>
2020-06-20 12:54:44 -0400

    added update and delete for comments, need auth next

--- Final/routes/comments.js
+++ Final/routes/comments.js
@@ -45,6 +45,41 @@ router.post("/", isLoggedIn,(req,res) => {
     })
 })
 
+// form to edit comment
+router.get("/:comment_id/edit", (req, res) => {
+    Comment.findById(req.params.comment_id, (err, foundComment) => {
+        if (err){
+            console.log(err);
+            res.redirect("back")
+        }
+        else{
+            res.render("comments/edit",{campground_id: req.params.id, comment: foundComment});
+        }
+    })
+})
+
+router.put("/:comment_id", (req, res) => {
+    Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, (err, updateComment) => {
+        if (err){
+            console.log(err);
+            res.redirect("back");
+        }
+        else{
+            res.redirect("/campgrounds/"+ req.params.id);
+        }
+    })
+})
+
+router.delete("/:comment_id", (req, res) => {
+    Comment.findByIdAndRemove(req.params.comment_id, (err) => {
+        if (err){
+            console.log(err);
+            res.redirect("back");
+        }
+        res.redirect("/campgrounds/"+req.params.id);
+    }) 
+})
+
 //middleware for checking authentication/ if user is logged in
 function isLoggedIn(req,res, next){
     if (req.isAuthenticated()){
